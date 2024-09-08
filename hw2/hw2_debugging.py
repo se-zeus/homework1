207b2c86b314070a42db5bf6b76cc4fb887ca1fe
Megh Dedhia <m11dedhia@gmail.com>
2024-09-08 21:34:03 +0000

    fixed merge sort and added pre bug-fixing trace

---
+++ hw2/hw2_debugging.py
@@ -0,0 +1,39 @@
+import rand
+
+
+def mergeSort(arr):
+    if (len(arr) == 1):
+        return arr
+
+    half = len(arr)//2
+
+    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+
+
+def recombine(leftArr, rightArr):
+    leftIndex = 0
+    rightIndex = 0
+    mergeArr = [None] * (len(leftArr) + len(rightArr))
+    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
+        if leftArr[leftIndex] < rightArr[rightIndex]:
+            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+            leftIndex += 1
+        else:
+            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+            rightIndex += 1
+
+    for i in range(rightIndex, len(rightArr)):
+        mergeArr[leftIndex + rightIndex] = rightArr[i]
+        rightIndex += 1
+
+    for i in range(leftIndex, len(leftArr)):
+        mergeArr[leftIndex + rightIndex] = leftArr[i]
+        leftIndex += 1
+
+    return mergeArr
+
+
+arr = rand.random_array([None] * 20)
+arr_out = mergeSort(arr)
+
+print(arr_out)

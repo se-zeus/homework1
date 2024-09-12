8cf00fdf684e58fcf6e44bb5a56bc60fe02383d5
Megh Dedhia <m11dedhia@gmail.com>
2024-09-12 03:52:43 +0000

    fixed security vulnerabilities from bandit and added new traces

--- hw2/rand.py
+++ hw2/rand.py
@@ -1,11 +1,6 @@
-import subprocess
-
+import secrets
 
 def random_array(arr):
-    shuffled_num = None
     for i in range(len(arr)):
-        shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+        arr[i] = secrets.randbelow(20) + 1
     return arr
-                    
